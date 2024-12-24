4e1bab206686df78073ace3729cc4e20127238bf
bhrtmali96 <bhrtwork96@gmail.com>
2024-12-25 01:08:25 +0530

    changed some data

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,27 @@
 const root = document.getElementById('container');
 
-const data = [
-    {
-        title:"ACSS CUMBERLAND",
-        desc:"ACSS CUMBERLAND 18TW/14TW/18TW/19ACS",
-        img:"assets/images/cumberland.jpg"
-    },
-    {
-        title:"GAP-516 SQMM",
-        desc:"GAP-516 SQMM 14TW/9TW/7ST(2.50)",
-        img:"assets/images/gap_516.jpg"
-    },
-    {
-        title:"ACCC GDANSK",
-        desc:"ACCC GDANSK 10TW/6TW/CTC(5.97MM)",
-        img:"assets/images/gdansk.jpg"
-    },
-    {
-        title:"GAP-593 SQMM",
-        desc:"GAP-593 SQMM 24RW/18RW/8TW/7ST(2.95)",
-        img:"assets/images/gap_593.jpg"
-    },
-    {
-        title:"ACCC CASAPLUSE",
-        desc:"ACCC CASAPLUSE 10TW/6TW/CTC(7.11)",
-        img:"assets/images/casapluse.jpg"
-    }
+async function setData(){
+    const response = await fetch('assets/data.json')
+    const data = await response.json();
 
-]
+    let htmlCode = "";
 
-let htmlCode = "";
-
-for(element of data){
-    let card = `<div class="card" >
-            <img src="${element.img}" alt="" class="card-img">
+    for (element of data) {
+        let card = `<div class="card" >
             <h1 class="card-title">${element.title}</h1>
             <p class="card-desc">${element.desc}</p>
+            <img src="${element.img}" alt="" class="card-img">
+            
             <div class="card-link">
                 <a href="${element.img}">See Photo</a>
             </div>
         </div>`
 
-    htmlCode = htmlCode + card
+        htmlCode = htmlCode + card
+    }
+
+    root.innerHTML = htmlCode;
+
 }
 
-root.innerHTML = htmlCode;
+setData();
